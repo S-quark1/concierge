e06a840bd5832ae79da44a343c3f2188d83c5652
S-quark1 <binarakuanysheva13@gmail.com>
2023-01-13 01:45:00 +0600

    css in admin page working

---
+++ cmd/api/admin.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+)
+
+func (app *application) AdminPageHandler(w http.ResponseWriter, r *http.Request) {
+	files := []string{
+		"./ui/pages/admin/admin-page.html",
+		"./ui/pages/admin/blank.html",
+		"./ui/pages/admin/404.html", // TODO и остальное...
+	}
+
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	// Use the ExecuteTemplate() method to write the content of the "base"
+	// template as the response body.
+
+	// The last parameter to Execute() represents any dynamic data that we
+	// want to pass in, which for now we'll leave as nil.
+	//err = ts.ExecuteTemplate(w, "base", "smth smth smth smth data")
+	err = ts.Execute(w, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+}
--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -8,16 +8,19 @@ import (
 func (app *application) routes() http.Handler {
 	router := httprouter.New()
 
+	router.ServeFiles("/css/*filepath", http.Dir("./ui/css/"))
 	router.ServeFiles("/img/*filepath", http.Dir("./ui/img/"))
 	router.ServeFiles("/js/*filepath", http.Dir("./ui/js/"))
 	router.ServeFiles("/pages/*filepath", http.Dir("./ui/pages/"))
 	router.ServeFiles("/scss/*filepath", http.Dir("./ui/scss/"))
 	router.ServeFiles("/vendor/*filepath", http.Dir("./ui/vendor/"))
 	// TODO если что нужно тут добавляем
+
 	// LandingPage
 	router.HandlerFunc(http.MethodGet, "/", app.LandingPageHandler)
 
 	// Admin
+	router.HandlerFunc(http.MethodGet, "/admin", app.AdminPageHandler)
 
 	// B2B
 
@@ -28,8 +31,8 @@ func (app *application) routes() http.Handler {
 	// Partner
 
 	//mux := http.NewServeMux()
-	//fileServer := http.FileServer(http.Dir("./ui/static/"))
-	//mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	//fileServer := http.FileServer(http.Dir("./ui/static(delete)/"))
+	//mux.Handle("/static(delete)/", http.StripPrefix("/static(delete)", fileServer))
 	//
 	//mux.HandleFunc("/", app.homePageHandler)
 	//mux.HandleFunc("/auth", app.authorizationPageHandler)
